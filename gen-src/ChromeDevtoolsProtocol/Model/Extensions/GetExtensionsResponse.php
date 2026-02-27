57a8eeadb92ac2cfc6f0d69efbfae078a097e2f3
Sam Feyaerts <sam@feyaerts.me>
2026-02-27 03:45:53 +0000

    autoupdated protocol.json & generated files

---
+++ gen-src/ChromeDevtoolsProtocol/Model/Extensions/GetExtensionsResponse.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ChromeDevtoolsProtocol\Model\Extensions;
+
+/**
+ * Response to Extensions.getExtensions command.
+ *
+ * @generated This file has been auto-generated, do not edit.
+ *
+ * @author Jakub Kulhan <jakub.kulhan@gmail.com>
+ */
+final class GetExtensionsResponse implements \JsonSerializable
+{
+	/** @var ExtensionInfo[] */
+	public $extensions;
+
+
+	/**
+	 * @param object $data
+	 * @return static
+	 */
+	public static function fromJson($data)
+	{
+		$instance = new static();
+		if (isset($data->extensions)) {
+			$instance->extensions = [];
+			foreach ($data->extensions as $item) {
+				$instance->extensions[] = ExtensionInfo::fromJson($item);
+			}
+		}
+		return $instance;
+	}
+
+
+	#[\ReturnTypeWillChange]
+	public function jsonSerialize()
+	{
+		$data = new \stdClass();
+		if ($this->extensions !== null) {
+			$data->extensions = [];
+			foreach ($this->extensions as $item) {
+				$data->extensions[] = $item->jsonSerialize();
+			}
+		}
+		return $data;
+	}
+}
