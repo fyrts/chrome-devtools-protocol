426b3cf26f08699799aa84a701d1b171829e976c
Sam Feyaerts <sam@feyaerts.me>
2026-08-19 03:38:37 +0000

    autoupdated protocol.json & generated files

--- gen-src/ChromeDevtoolsProtocol/Domain/EmulationDomain.php
+++ gen-src/ChromeDevtoolsProtocol/Domain/EmulationDomain.php
@@ -14,6 +14,7 @@ use ChromeDevtoolsProtocol\Model\Emulation\RemoveScreenRequest;
 use ChromeDevtoolsProtocol\Model\Emulation\ScreenOrientationLockChangedEvent;
 use ChromeDevtoolsProtocol\Model\Emulation\SetAutoDarkModeOverrideRequest;
 use ChromeDevtoolsProtocol\Model\Emulation\SetAutomationOverrideRequest;
+use ChromeDevtoolsProtocol\Model\Emulation\SetCPUPerformanceOverrideRequest;
 use ChromeDevtoolsProtocol\Model\Emulation\SetCPUThrottlingRateRequest;
 use ChromeDevtoolsProtocol\Model\Emulation\SetDataSaverOverrideRequest;
 use ChromeDevtoolsProtocol\Model\Emulation\SetDefaultBackgroundColorOverrideRequest;
@@ -156,6 +157,15 @@ class EmulationDomain implements EmulationDomainInterface
 	public function setAutomationOverride(ContextInterface $ctx, SetAutomationOverrideRequest $request): void
 	{
 		$this->internalClient->executeCommand($ctx, 'Emulation.setAutomationOverride', $request);
+	}
+
+
+	public function setCPUPerformanceOverride(
+		ContextInterface $ctx,
+		?SetCPUPerformanceOverrideRequest $request = null
+	): void {
+		if (is_null($request)) $request = new \stdClass();
+		$this->internalClient->executeCommand($ctx, 'Emulation.setCPUPerformanceOverride', $request);
 	}
 
 
